b27b29920025974015893441c631d34ac84798fe
Musab Hamid <47342534+musab118@users.noreply.github.com>
2021-01-31 19:25:14 -0600

    implemented linked list

---
+++ linked_list.js
@@ -0,0 +1,36 @@
+//Basic Linked list exxample
+
+class LinkedList {
+    constructor(value) {
+      this.head = {
+        value: value,
+        next: null,
+      };
+      this.tail = this.head;
+      this.length = 1;
+    }
+    append(value) {
+      const newNode = {
+        value: value,
+        next: null,
+      };
+      this.tail.next = newNode;
+      this.tail = newNode;
+      this.length++;
+    }
+    prepend(value) {
+      const firstNode = {
+        value: value,
+        next: null,
+      };
+      firstNode.next = this.head;
+      this.head = firstNode;
+      this.length++;
+    }
+  }
+  
+  const myLinkedList = new LinkedList(10);
+  myLinkedList.append(5);
+  myLinkedList.append(16);
+  myLinkedList.prepend(1);
+  myLinkedList.prepend(4);
