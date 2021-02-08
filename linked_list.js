4460b38e0a20a0f2245d298c8efc7a8791fd0881
Musab Hamid <47342534+musab118@users.noreply.github.com>
2021-02-08 17:44:13 -0600

    Lists and Stacks

--- linked_list.js
+++ linked_list.js
@@ -1,36 +1,81 @@
 //Basic Linked list exxample
 
 class LinkedList {
-    constructor(value) {
-      this.head = {
-        value: value,
-        next: null,
-      };
-      this.tail = this.head;
-      this.length = 1;
-    }
-    append(value) {
-      const newNode = {
-        value: value,
-        next: null,
-      };
-      this.tail.next = newNode;
-      this.tail = newNode;
-      this.length++;
-    }
-    prepend(value) {
-      const firstNode = {
-        value: value,
-        next: null,
-      };
-      firstNode.next = this.head;
-      this.head = firstNode;
-      this.length++;
-    }
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+    };
+    this.tail = this.head;
+    this.length = 1;
   }
-  
-  const myLinkedList = new LinkedList(10);
-  myLinkedList.append(5);
-  myLinkedList.append(16);
-  myLinkedList.prepend(1);
-  myLinkedList.prepend(4);
+  append(value) {
+    const newNode = {
+      value: value,
+      next: null,
+    };
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+  }
+
+  prepend(value) {
+    const firstNode = {
+      value: value,
+      next: null,
+    };
+    firstNode.next = this.head;
+    this.head = firstNode;
+    this.length++;
+  }
+
+  insert(index, value) {
+    if (index === 0) {
+      this.prepend(value);
+    } else if (index >= this.length) {
+      this.append(value);
+    }
+    const newNode = {
+      value: value,
+      next: null,
+    };
+    const leadingNode = this.traverseToIndex(index - 1); 
+    const nextPointer = leadingNode.next;
+    leadingNode.next = newNode;
+    newNode.next = nextPointer
+    this.length++
+    
+  }
+
+  traverseToIndex(index) {
+    // check params
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode
+  }
+
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    console.log(array);
+  }
+
+  remove(index){
+   
+  // Check if head or tail values are being removed
+    const leader = this.traverseToIndex(index-1)
+    const unwantedNode = leader.next
+    leader.next = unwantedNode.next;
+    this.length--
+    return this.printList()
+
+  }
+}
